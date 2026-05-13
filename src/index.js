a9a59c135bab0869e0d42a69fdc1375aa1cf9cf6
Arda Şahin <ardasahin.personel@gmail.com>
2026-05-13 12:06:40 +0300

    Docker files have been added and support for the build process has been added.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.get('/calculate/add/:num1/:num2', (req, res) => {
+    const num1 = parseFloat(req.params.num1);
+    const num2 = parseFloat(req.params.num2);
+    const result = num1 + num2;
+    res.send(`The result of ${num1} + ${num2} is ${result}`);
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
 })
